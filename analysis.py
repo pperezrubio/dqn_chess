a41410a19a3b9cdb16e424fc6a150a0ac8990167
Jacob Menick <jmenick@reed.edu>
2015-05-14 23:10:49 -0700

    update dqn

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ from chess import ChessGame
 import numpy as np
 from matplotlib import pyplot as plt
 from sunfish.xboard import parseFEN
+from execution import PIECE_DICT
 
 # function definitions
 # parse 
@@ -17,20 +18,19 @@ def render_state(fen,c=1):
 
 # MAIN
 g = ChessGame()
-N = 20
+g.
 
-# run 20 random games.
-for i in range(N):
-    j = 0
-    while not g.is_terminal():
-        j += 1
-        a = np.random.randint(g.n_actions)
-        r = g.take_action(a)
-        if j % 50 == 0:
-            print "game: %d, iteration: %d, action: %d, reward: %d, status: %d" %(
-                i, j, a, r, g._STATUS
-            )
-    print "game end: %d" %g._STATUS
-    g.reset()
 
+# run 1 random game.
+j = 0
+while not g.is_terminal():
+    j += 1
+    a = np.random.randint(g.n_actions)
+    r = g.take_action(a)
+    if j % 50 == 0:
+        print "iteration: %d, action: %d, reward: %d, status: %d" %(
+            j, a, r, g._STATUS
+        )
+print "game end: %d" %g._STATUS
+ 
  
